3bcccd5e7a4dfc8f68fdc7365fa894d00888d913
patrick Wiley <wileypatrick36@yahoo.com>
2019-09-26 01:08:48 +0200

    change tableViewController -> viewController

--- RuckSack/Controller/TranslatorViewController.swift
+++ RuckSack/Controller/TranslatorViewController.swift
@@ -12,50 +12,51 @@ class TranslatorViewController: UIViewController {
     
     var translate = Translate()
     
-    @IBOutlet var tableViewTranslator: UITableView!
+    @IBOutlet var translatorViewController: UIView!
+    @IBOutlet weak var flagLanguageOrigin: UIImageView!
+    @IBOutlet weak var titleLanguageOrigin: UILabel!
+    @IBOutlet weak var textLanguageOrigin: UITextView!
+    
+    @IBOutlet weak var flagLanguageDestination: UIImageView!
+    @IBOutlet weak var titleLanguageDestination: UILabel!
+    @IBOutlet weak var textLanguageDestination: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //tableViewTranslator.estimatedRowHeight = 200
         translate.languages = translate.createLanguage()
         
-
+        
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
-        tableViewTranslator.backgroundView = UIImageView(image: UIImage(named: "Background_Translator"))
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Background_Translator")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        translatorViewController.insertSubview(backgroundImage, at: 0)
+        navigationController?.hidesBarsWhenKeyboardAppears = true
+        
+        let bar = UIToolbar()
+        let done = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(resetTapped))
+        bar.items = [done]
+        bar.sizeToFit()
+        textLanguageOrigin.inputAccessoryView = bar
+        textLanguageDestination.inputAccessoryView = bar
+        
         
     }
-
-   
-
-}
-
-extension TranslatorViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return translate.languages.count
+    @objc func resetTapped() {
+        if textLanguageOrigin.isEditable == true {
+            print("YES it s amountOrigin")
+            textLanguageOrigin.isEditable = false
+        } else if textLanguageDestination.isEditable == true{
+            print("YES it s amountDestination")
+            textLanguageDestination.isEditable = false
+        }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let language = translate.languages[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LanguageCell") as! LanguageCell
-        cell.setLanguage(language: language)
-        return cell
-    }
-//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-//        if (text == "\n")
-//        {
-//            
-//            self.view.endEditing(true);
-//            return false;
-//        }
-//        return true
-//    }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-        
-    }
 }
