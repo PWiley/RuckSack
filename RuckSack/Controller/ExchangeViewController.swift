94bfd1d15edd293305bdcd1c011b588a1aa92641
patrick Wiley <wileypatrick36@yahoo.com>
2019-09-26 01:09:51 +0200

    change tableViewController -> viewController

--- RuckSack/Controller/ExchangeViewController.swift
+++ RuckSack/Controller/ExchangeViewController.swift
@@ -11,38 +11,72 @@ import UIKit
 class ExchangeViewController: UIViewController {
     var exchange = Exchange()
     
-    @IBOutlet var tableViewExchange: UITableView!
+    @IBOutlet var exchangeViewController: UIView!
     
+    @IBOutlet weak var flagCurrencyOrigin: UIImageView!
+    @IBOutlet weak var shortLabelOrigin: UILabel!
+    @IBOutlet weak var labelOrigin: UILabel!
+    @IBOutlet weak var amountOrigin: UITextField!
+    
+    @IBOutlet weak var flagCurrencyDestination: UIImageView!
+    @IBOutlet weak var shortLabelDestination: UILabel!
+    @IBOutlet weak var labelDestination: UILabel!
+    @IBOutlet weak var amountDestination: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //tableViewExchange.estimatedRowHeight = 120
+        
         exchange.currencies = exchange.createCurrency()
         
         // Do any additional setup after loading the view.
     }
+    fileprivate func extractedFunc() {
+        // MARK: set the informations for the two views
+        
+        flagCurrencyOrigin.image = exchange.currencies[0].image
+        shortLabelOrigin.text = exchange.currencies[0].shortLabel
+        labelOrigin.text = exchange.currencies[0].name
+        amountOrigin.text = exchange.currencies[0].amount
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
-        tableViewExchange.backgroundView = UIImageView(image: UIImage(named: "Background_Exchange"))
-        //tableViewExchange.invalidateIntrinsicContentSize()
-    }
-    
-    
-}
-
-extension ExchangeViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return exchange.currencies.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currency = exchange.currencies[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ExchangeCell") as! ExchangeCell
-        cell.setCurrency(currency: currency)
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
         
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Background_Exchange")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        exchangeViewController.insertSubview(backgroundImage, at: 0)
+        navigationController?.hidesBarsWhenKeyboardAppears = true
+        
+        let bar = UIToolbar()
+        let done = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(resetTapped))
+        bar.items = [done]
+        bar.sizeToFit()
+        amountDestination.inputAccessoryView = bar
+        amountOrigin.inputAccessoryView = bar
+        
+        
+        setCurrency(currency: exchange.currencies[0])
+        
+        flagCurrencyDestination.image = exchange.currencies[1].image
+        shortLabelDestination.text = exchange.currencies[1].shortLabel
+        labelDestination.text = exchange.currencies[1].name
+        amountDestination.text = exchange.currencies[1].amount
+        
+    }
+    @objc func resetTapped() {
+        if amountOrigin.isEditing == true {
+            print("YES it s amountOrigin")
+        }
+        if amountDestination.isEditing == true {
+            print("YES it s amountDestination")
+        }
+    }
+    func setCurrency(currency: Currency) {
+        
+        flagCurrencyDestination.image = exchange.currencies[1].image
+        shortLabelDestination.text = exchange.currencies[1].shortLabel
+        labelDestination.text = exchange.currencies[1].name
+        amountDestination.text = exchange.currencies[1].amount
     }
 }
