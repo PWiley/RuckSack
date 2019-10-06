768fa140978786fb43dd4ce983b163ab02e9b0c5
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-06 20:28:59 +0200

    add request handling

--- RuckSack/Model/Exchange.swift
+++ RuckSack/Model/Exchange.swift
@@ -11,24 +11,36 @@ import UIKit
 
 class CurrencyModel {
     
-//    var currencies: [Currency] = []
-//
-//    func createCurrency() -> [Currency] {
-//        let euro = Currency(shortLabel: "EUR", name: "Euro", image: #imageLiteral(resourceName: "british-pound"), amount: "1000")
-//        let dollar = Currency(shortLabel: "USD", name: "US-Dollar", image: #imageLiteral(resourceName: "us-dollar"), amount: "1000")
-//         return [euro, dollar]
-//    }
-    func askCurrencyRate() {
+    private static let currencyURL = URL(string: "http://data.fixer.io/api/latest?")!
+    private var task: URLSessionDataTask?
+    var currency: Currency?
+    
+//private static let urlString = "http://data.fixer.io/api/latest?"
+    func createRequest() -> URLRequest {
+
         
-        let fixerString = "https://data.fixer.io/api/latest?access_key=512b6a9fdea5eb6241c0cda88a1079eb"
-        guard let fixerURL = URL(string: fixerString) else { return }
+        let body = "access_key=512b6a9fdea5eb6241c0cda88a1079eb&base=EUR&symbols=USD"
+        
+        var request = URLRequest(url: CurrencyModel.currencyURL)
+        request.httpMethod = "POST"
+        request.httpBody = body.data(using: .utf8)
+//        print(request.httpBody as Any)
+//        print(request)
+        return request
+    }
+    
+    func askCurrencyRate(){
+        
+        
+        //guard let fixerURL = URL(string: CurrencyModel.urlString) else { return }
+        let request = createRequest()
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: fixerURL) { data, response, error in
+        task = session.dataTask(with: request) { data, response, error in
             if error != nil {
                 print("Client error!")
                 return
             }
-            guard let data = data else {
+            guard let jsonData = data else {
                 print("Error data!")
                 return
             }
@@ -41,38 +53,30 @@ class CurrencyModel {
                 print("Wrong MIME type!")
                 return
             }
+            print(jsonData)
             do {
-                let currency = try JSONDecoder().decode(Currency.self, from: data)
-                print(currency.rates)
+                self.currency = try? JSONDecoder().decode(Currency.self, from: jsonData)
+                print("Ouhra")
+//                self.requestData(currency: self.currency!)
+
             } catch {
                 print("JSON error")
             }
-        }.resume()
-       
+           
+        }
+        task?.resume()
     }
     
-    
+    var delegate: CurrencyModelDelegate?
+    func requestData(currency: Currency) {
+        // the data was received and parsed to String
+        
+        self.delegate?.didRecieveDataUpdate(data: currency)
+    }
+
 }
 
-//class Currency {
-//
-//    var shortLabel: String
-//    var name: String
-//    var image: UIImage
-//    var amount: String
-//
-//    init(shortLabel: String, name: String, image: UIImage, amount: String) {
-//        self.shortLabel = shortLabel
-//        self.name = name
-//        self.image = image
-//        self.amount = amount
-//    }
-//}
 
-
-struct Currency: Codable {
-    let success: Bool
-    let timestamp: Int
-    let base, date: String
-    let rates: [String: Double]
+protocol CurrencyModelDelegate {
+func didRecieveDataUpdate(data: Currency)
 }
