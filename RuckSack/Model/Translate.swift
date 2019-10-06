474f0605b727b493526582537d1688d3984b3898
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-06 20:34:49 +0200

    add network call

--- RuckSack/Model/Translate.swift
+++ RuckSack/Model/Translate.swift
@@ -6,35 +6,46 @@
 //  Copyright © 2019 Patrick Wiley. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import Foundation
 
-class Translate {
+class TranslateModel {
+// Get the file
+private let translateURL = URL(string: "https://translation.googleapis.com/language/translate/v2/&key=AIzaSyDd_8AuTs9gGs_jA233qUPv2_P69qtnW7c")!
+   
+    func createJson() -> Data {
+        let translation = TranslationToSend(q: ["Hello world, it's time to go far away"], target: "de")
+
+        let json = try? JSONEncoder().encode(translation)
+        let jsonString = String(data: json!, encoding: .utf8)
+
+        print(jsonString)
+        print(json)
+        return json!
+    }
+    func createCall(jsonData: Data ) {
+        
+        var request = URLRequest(url: translateURL)
+        request.httpMethod = "POST"
+        request.httpBody = createJson()
+        
+        
+        //let session = URLSession(configuration: .default)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print("Hourra")
+            guard let data = data, error == nil else { return}
+            let jsonAnswerTranslation = try? JSONDecoder().decode(Translation.self , from: data)
+            let jsonAnswerDataClass = try? JSONDecoder().decode(DataClass.self , from: data)
+            let jsonAnswerTranslationElement = try? JSONDecoder().decode(TranslationElement.self , from: data)
+        }.resume()
+    }
+ 
     
-//    var languages: [Language] = []
-//    
-//    
-//    func createLanguage() -> [Language] {
-//        let french = Language(image: #imageLiteral(resourceName: "France"), title: "France", text: "Tapez votre texte!")
-//        let english = Language(image: #imageLiteral(resourceName: "Great Britain"), title: "United Kingdom", text: "Enter your text!")
-//        
-//        return [french,english]
-//    }
-    
+
+
+
+
 }
-
-//class Language {
-//
-//
-//    var image: UIImage
-//    var title: String
-//    var text: String
-//
-//    init(image: UIImage, title: String, text: String) {
-//        self.image = image
-//        self.title = title
-//        self.text = text
-//    }
-//
-//}
-
+//let translate = Translate()
+//var jsonToUse = translate.createJson()
+//translate.createCall(jsonData: jsonToUse)
