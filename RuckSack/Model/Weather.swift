4e916d44b41d73c9eb635ecfb1adc92a1026cd32
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-06 20:36:28 +0200

    change network call

--- RuckSack/Model/Weather.swift
+++ RuckSack/Model/Weather.swift
@@ -8,164 +8,67 @@
 
 import Foundation
 
-// MARK: Structures JSON Data
-// MARK: - Exchange
-struct Exchange: Codable {
-    let cod: String
-    let message: Double
-    let cnt: Int
-    let list: [List]
-    let city: City
-}
-
-// MARK: - City
-struct City: Codable {
-    let id: Int
-    let name: String
-    let coord: Coord
-    let country: String
-    let population, timezone, sunrise, sunset: Int
-}
-
-// MARK: - Coord
-struct Coord: Codable {
-    let lat, lon: Double
-}
-
-// MARK: - List
-struct List: Codable {
-    let dt: Int
-    let main: MainClass
-    let weather: [Weather]
-    let clouds: Clouds
-    let wind: Wind
-    let sys: Sys
-    let dtTxt: String
-    let rain: Rain?
-
-    enum CodingKeys: String, CodingKey {
-        case dt, main, weather, clouds, wind, sys
-        case dtTxt
-        case rain
-    }
-}
-
-// MARK: - Clouds
-struct Clouds: Codable {
-    let all: Int
-}
-
-// MARK: - MainClass
-struct MainClass: Codable {
-    let temp, tempMin, tempMax, pressure: Double
-    let seaLevel, grndLevel: Double
-    let humidity: Int
-    let tempKf: Double
-
-    enum CodingKeys: String, CodingKey {
-        case temp
-        case tempMin
-        case tempMax
-        case pressure
-        case seaLevel
-        case grndLevel
-        case humidity
-        case tempKf
-    }
-}
-
-// MARK: - Rain
-struct Rain: Codable {
-    let the3H: Double
-
-    enum CodingKeys: String, CodingKey {
-        case the3H
-    }
-}
-
-// MARK: - Sys
-struct Sys: Codable {
-    let pod: Pod
-}
-
-enum Pod: String, Codable {
-    case d = "d"
-    case n = "n"
-}
-
-// MARK: - Weather
-struct Weather: Codable {
-    let id: Int
-    let main: MainEnum
-    let weatherDescription: Description
-    let icon: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, main
-        case weatherDescription
-        case icon
-    }
-}
-
-enum MainEnum: String, Codable {
-    case clear = "Clear"
-    case clouds = "Clouds"
-    case rain = "Rain"
-}
-
-enum Description: String, Codable {
-    case brokenClouds = "broken clouds"
-    case clearSky = "clear sky"
-    case fewClouds = "few clouds"
-    case lightRain = "light rain"
-    case scatteredClouds = "scattered clouds"
-}
-
-// MARK: - Wind
-struct Wind: Codable {
-    let speed, deg: Double
-}
-
 
 class WeatherModel {
     
-    func askWeatherState() {
-        
-        
-        let WeatherURL = URL(string: "http://api.openweathermap.org/data/2.5/forecast?q=Paris,us&mode=json&appid=d2fc02766020f446cb8063c244166041")!
-        //let session = URLSession(configuration: .default)
-        //var request = URLRequest(url: fixerURL)
-        //request.httpMethod = "GET"
-        let task = URLSession.shared.dataTask(with: WeatherURL) { data, response, error in
-            if error != nil {
-                print("Client error!")
-                return
-            }
-            guard let data = data else {
-                print("Error data!")
-                return
-            }
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                print("Server error!")
-                return
-            }
+    static let baseURL = "https://api.openweathermap.org/data/2.5/forecast?"
+    static let apiKey = "q=Paris,us&mode=json&appid=d2fc02766020f446cb8063c244166041"
+    
+    private static let weatherURL = URL(string: baseURL + apiKey)!
+    private var task: URLSessionDataTask?
+    var exchange: Exchange?
+    var list: List?
+    var main: MainClass?
+    
+    
+        func askWeatherState() {
             
-            guard let mime = response.mimeType, mime == "application/json" else {
-                print("Wrong MIME type!")
-                return
+            let request = URLRequest(url: WeatherModel.weatherURL)
+            let session = URLSession(configuration: .default)
+            task = session.dataTask(with: request) { data, response, error in
+            //print(response)
+                if error != nil {
+                    print("Client error!")
+                    print("something went wrong ", error!)
+                    return
+                }
+                guard let jsonData = data else {
+                    print("Error data!")
+                    
+                    return
+                }
+                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                    print("Server error!")
+                    return
+                }
+                
+                guard let mime = response.mimeType, mime == "application/json" else {
+                    print("Wrong MIME type!")
+                    return
+                }
+                print(jsonData)
+                do {
+                    
+                    self.exchange = try? JSONDecoder().decode(Exchange.self, from: jsonData)
+                    self.list = try? JSONDecoder().decode(List.self, from: jsonData)
+                    self.main = try? JSONDecoder().decode(MainClass.self, from: jsonData)
+                    print("Ouhra")
+                
+                } catch {
+                    
+                    print("JSON error")
+                }
+               
             }
-
-            do {
-                let  weatherJson = try JSONDecoder().decode(Exchange.self, from: data)
-                print(weatherJson)
-            } catch {
-                print("JSON error: \(error.localizedDescription)")
-            }
+            task?.resume()
         }
-        task.resume()
-        
-
-    }
     
 
+}
+
+
+enum NetworkError: Error {
+    case clientError
+    case serverError
+    case jsonError
 }
