7a83603192159555c61cc7c0ff8a054291ca7c59
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-16 22:35:21 +0200

    add request handling

--- RuckSack/Model/Exchange.swift
+++ RuckSack/Model/Exchange.swift
@@ -7,82 +7,84 @@
 //
 
 import Foundation
-import UIKit
+//import UIKit
 
 class CurrencyModel {
     
-    weak var delegate: CurrencyModelDelegate?
-    
-    private static let currencyURL = URL(string: "https://data.fixer.io/api/latest?")!
+    private static let currencyURL = URL(string: "http://data.fixer.io/api/latest?")!
     private var task: URLSessionDataTask?
     var currency: Currency?
+    var delegate: CurrencyModelDelegate?
     
-//private static let urlString = "http://data.fixer.io/api/latest?"
+    var exchangeView: ExchangeViewController?
+    
     func createRequest() -> URLRequest {
-
         
-        //let body = "access_key=512b6a9fdea5eb6241c0cda88a1079eb&base=EUR&symbols=USD"
-        
-        let query = ["access_key": "512b6a9fdea5eb6241c0cda88a1079eb", "base": "EUR", "symbols": "USD"]
+        let query: [String: String] = [
+            "access_key": "512b6a9fdea5eb6241c0cda88a1079eb",
+            "symbols": "USD",
+            "base": "EUR"
+        ]
         
         var request = URLRequest(url: CurrencyModel.currencyURL.withQueries(query)!)
-        request.httpMethod = "GET"
-        
-        //request.httpBody = body.data(using: .utf8)
-//        print(request.httpBody as Any)
-//        print(request)
+        request.httpMethod = "POST"
+        print(request)
         return request
     }
     
     func askCurrencyRate(){
         
-        
-        //guard let fixerURL = URL(string: CurrencyModel.urlString) else { return }
+        task?.cancel()
         let request = createRequest()
         let session = URLSession(configuration: .default)
         task = session.dataTask(with: request) { data, response, error in
-            guard let jsonData = data, error == nil else {
-                print("Client error!", error)
+            if error != nil {
+                print("Client error!")
                 return
             }
-            
+            guard let jsonData = data else {
+                print("Error data!")
+                return
+            }
             guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
                 print("Server error!")
                 return
             }
-            
-            guard let mime = response.mimeType, mime == "application/json" else {
-                print("Wrong MIME type!")
-                return
+            DispatchQueue.main.async {
+                do {
+                    
+                    self.currency = try JSONDecoder().decode(Currency.self, from: jsonData)
+                    //print("Ouhra: currency = \(self.currency?.rates?.usd)")
+                    self.requestData(currency: self.currency!)
+                    
+                } catch {
+                    print("JSON error: \(error)")
+                }
             }
-            print(jsonData)
-            
-//            self.currency = try? JSONDecoder().decode(Currency.self, from: jsonData)
-//            print("Ouhra")
-//            self.requestData(currency: self.currency!)
-            do {
-                self.currency = try? JSONDecoder().decode(Currency.self, from: jsonData)
-                print("Ouhra")
-                self.requestData(currency: self.currency!)
-
-            } catch {
-                print("JSON error")
-            }
-           
         }
         task?.resume()
     }
-   
+    
     
     func requestData(currency: Currency) {
         // the data was received and parsed to String
         
-        self.delegate?.didRecieveDataUpdate(data: currency)
+        self.delegate?.didUpdateCurrencyData(data: currency)
+        //calculateConversion(rates: (describing: currency.rates))
     }
+    
+    func calculateConversion(rates: Double) {
+        if exchangeView?.amountOrigin.text == "" {
+            print("AmountOrigin is empty")
+        } else {
+            print("'AmountDestination is empty")
+        }
 
+    }
+    
 }
 
 
-protocol CurrencyModelDelegate: class {
-func didRecieveDataUpdate(data: Currency)
+protocol CurrencyModelDelegate {
+    func didUpdateCurrencyData(data: Currency)
 }
