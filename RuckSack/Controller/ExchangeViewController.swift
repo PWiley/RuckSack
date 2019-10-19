75e979304f009912c3763769903423be356465c1
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-20 00:25:42 +0200

    no more delegate and handling backgroundTown

--- RuckSack/Controller/ExchangeViewController.swift
+++ RuckSack/Controller/ExchangeViewController.swift
@@ -8,57 +8,72 @@
 
 import UIKit
 
-class ExchangeViewController: UIViewController, CurrencyModelDelegate {
+class ExchangeViewController: UIViewController {
     
     let currencyModel = CurrencyModel()
     
+    
     @IBOutlet var exchangeViewController: UIView!
     
+    @IBOutlet weak var viewOrigin: DesignableView!
     @IBOutlet weak var flagCurrencyOrigin: UIImageView!
     @IBOutlet weak var shortLabelOrigin: UILabel!
     @IBOutlet weak var labelOrigin: UILabel!
     @IBOutlet weak var amountOrigin: UITextField!
     
+    @IBOutlet weak var viewDestination: DesignableView!
     @IBOutlet weak var flagCurrencyDestination: UIImageView!
     @IBOutlet weak var shortLabelDestination: UILabel!
     @IBOutlet weak var labelDestination: UILabel!
     @IBOutlet weak var amountDestination: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        currencyModel.delegate = self
+        //currencyModel.delegate = self
         currencyModel.askCurrencyRate()
-    
+        setBackGroundTown()
+        
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    fileprivate func setBackGroundTown() {
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        if WeatherViewController.whichTown == true {
+            backgroundImage.image = UIImage(named: "Background_Exchange_Berlin")
+        } else {
+            backgroundImage.image = UIImage(named: "Background_Exchange_NewYork")
+        }
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        exchangeViewController.insertSubview(backgroundImage, at: 0)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         
         super.viewWillAppear(animated)
         
-        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-        //backgroundImage.image = UIImage(named: "Background_Exchange_Berlin")
-        //backgroundImage.image = UIImage(named: "Background_Exchange_NewYork")
-        
-        if WeatherViewController.whichTown == true {
-        backgroundImage.image = UIImage(named: "Background_Translator_Berlin")
-        } else {
-        backgroundImage.image = UIImage(named: "Background_Translator_NewYork")
-        }
-        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
-        exchangeViewController.insertSubview(backgroundImage, at: 0)
-        addDoneButtonOnKeyboard()
+        setBackGroundTown()
+addDoneButtonOnKeyboard()
                 
     }
-    func didUpdateCurrencyData(data: Currency) {
-        print(currencyModel.currency?.rates)
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setBackGroundTown()
+        addDoneButtonOnKeyboard()
     }
+//    func didUpdateCurrencyData(data: Currency) {
+//        print(currencyModel.currency?.rates as Any)
+//    }
     func addDoneButtonOnKeyboard()
     {
         let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
         doneToolbar.barStyle = .default
         
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonAction))
         
         let items = [flexSpace, done]
         doneToolbar.items = items
@@ -73,9 +88,17 @@ class ExchangeViewController: UIViewController, CurrencyModelDelegate {
         self.amountOrigin.resignFirstResponder()
         self.amountDestination.resignFirstResponder()
         let currency = CurrencyModel()
+        
+        if amountOrigin.text != nil {
+            guard let amountDouble = Double(amountOrigin.text!) else {return}
+            currencyModel.calculateConversion(amount: amountDouble)
+        } else {
+            guard let amountDouble = Double(amountDestination.text!) else {return}
+            currencyModel.calculateConversion(amount: amountDouble)
+        }
+        
+        
         currency.askCurrencyRate()
-        print(currency.exchangeView?.amountOrigin.text)
-        currency.amountOrigin = currency.exchangeView?.amountOrigin.text
-        print("The amountOrigin is :\(currency.amountOrigin)")
+        
     }
 }
