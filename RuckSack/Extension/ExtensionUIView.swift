aa24c76a7afe6b605bd2c551ea7cdde7c98964e3
patrick Wiley <wileypatrick36@yahoo.com>
2019-10-20 22:57:41 +0200

    comment what no need

--- RuckSack/Extension/ExtensionUIView.swift
+++ RuckSack/Extension/ExtensionUIView.swift
@@ -37,33 +37,33 @@ extension UIView {
         }
     }
     
-//    @IBInspectable
-//    var borderWidth: CGFloat {
-//        get {
-//            return layer.borderWidth
-//        }
-//        set {
-//            layer.borderWidth = newValue
-//        }
-//    }
-//    
-//    @IBInspectable
-//    var borderColor: UIColor? {
-//        get {
-//            if let color = layer.borderColor {
-//                return UIColor(cgColor: color)
-//            }
-//            return nil
-//        }
-//        set {
-//            if let color = newValue {
-//                layer.borderColor = color.cgColor
-//            } else {
-//                layer.borderColor = nil
-//            }
-//        }
-//    }
-//    
+    //    @IBInspectable
+    //    var borderWidth: CGFloat {
+    //        get {
+    //            return layer.borderWidth
+    //        }
+    //        set {
+    //            layer.borderWidth = newValue
+    //        }
+    //    }
+    //    
+    //    @IBInspectable
+    //    var borderColor: UIColor? {
+    //        get {
+    //            if let color = layer.borderColor {
+    //                return UIColor(cgColor: color)
+    //            }
+    //            return nil
+    //        }
+    //        set {
+    //            if let color = newValue {
+    //                layer.borderColor = color.cgColor
+    //            } else {
+    //                layer.borderColor = nil
+    //            }
+    //        }
+    //    }
+    //    
     @IBInspectable
     var shadowRadius: CGFloat {
         get {
@@ -74,40 +74,53 @@ extension UIView {
         }
     }
     
-//    @IBInspectable
-//    var shadowOpacity: Float {
-//        get {
-//            return layer.shadowOpacity
-//        }
-//        set {
-//            layer.shadowOpacity = newValue
-//        }
-//    }
-//
-//    @IBInspectable
-//    var shadowOffset: CGSize {
-//        get {
-//            return layer.shadowOffset
-//        }
-//        set {
-//            layer.shadowOffset = newValue
-//        }
-//    }
+    //    @IBInspectable
+    //    var shadowOpacity: Float {
+    //        get {
+    //            return layer.shadowOpacity
+    //        }
+    //        set {
+    //            layer.shadowOpacity = newValue
+    //        }
+    //    }
+    //
+    //    @IBInspectable
+    //    var shadowOffset: CGSize {
+    //        get {
+    //            return layer.shadowOffset
+    //        }
+    //        set {
+    //            layer.shadowOffset = newValue
+    //        }
+    //    }
     
-//    @IBInspectable
-//    var shadowColor: UIColor? {
-//        get {
-//            if let color = layer.shadowColor {
-//                return UIColor(cgColor: color)
-//            }
-//            return nil
-//        }
-//        set {
-//            if let color = newValue {
-//                layer.shadowColor = color.cgColor
-//            } else {
-//                layer.shadowColor = nil
-//            }
-//        }
-//    }
+    //    @IBInspectable
+    //    var shadowColor: UIColor? {
+    //        get {
+    //            if let color = layer.shadowColor {
+    //                return UIColor(cgColor: color)
+    //            }
+    //            return nil
+    //        }
+    //        set {
+    //            if let color = newValue {
+    //                layer.shadowColor = color.cgColor
+    //            } else {
+    //                layer.shadowColor = nil
+    //            }
+    //        }
+    //    }
+    func fadeIn() {
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    func fadeOut() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            self.alpha = 0.0
+        }, completion: nil)
+    }
+    
 }
