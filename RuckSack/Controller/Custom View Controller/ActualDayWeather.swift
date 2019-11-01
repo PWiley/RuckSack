3b15f57a1bb2bbbfe4d1fe31b8b60031bf66bc63
patrick Wiley <wileypatrick36@yahoo.com>
2019-11-01 21:21:45 +0100

    Add ActualDayWeather

---
+++ RuckSack/Controller/Custom View Controller/ActualDayWeather.swift
@@ -0,0 +1,42 @@
+//
+//  ActualDayWeather.swift
+//  RuckSack
+//
+//  Created by Patrick Wiley on 01.11.19.
+//  Copyright © 2019 Patrick Wiley. All rights reserved.
+//
+
+import UIKit
+
+
+class ActualDayWeather: UIView {
+    
+    @IBOutlet var actualDayWeather: UIView!
+    @IBOutlet weak var imageActualWeather: UIImageView!
+    @IBOutlet weak var weatherDescription: UILabel!
+    @IBOutlet weak var tempActual: UILabel!
+    @IBOutlet weak var tempMax: UILabel!
+    @IBOutlet weak var tempMin: UILabel!
+   
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    override init(frame: CGRect) { // for using CustomView in code
+        super.init(frame: frame)
+        commonInit()
+    }
+    required init?(coder aDecoder: NSCoder) { // for using CustomView in IB
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    private func commonInit() {
+        Bundle.main.loadNibNamed("ActualDayWeather", owner: self, options: nil)
+        addSubview(actualDayWeather)
+        actualDayWeather.frame = self.bounds
+        actualDayWeather.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
