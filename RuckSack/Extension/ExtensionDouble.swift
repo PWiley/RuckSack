ad6077c517da48e769f9ae6ae901071bcb45b176
patrick Wiley <wileypatrick36@yahoo.com>
2019-11-09 00:21:37 +0100

    Erase clean extension

--- RuckSack/Extension/ExtensionDouble.swift
+++ RuckSack/Extension/ExtensionDouble.swift
@@ -9,11 +9,8 @@
 import Foundation
 
 extension Double {
-    var clean: String {
-        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
-    }
     
-    var celcius: Double {
+        var celcius: Double {
         let tempCelcius = self - 273.15
         return tempCelcius
     }
