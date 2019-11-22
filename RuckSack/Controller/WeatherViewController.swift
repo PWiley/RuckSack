9af6238f9f90492f814a58315bc4de22d470b1e6
patrick Wiley <wileypatrick36@yahoo.com>
2019-11-22 11:09:24 +0100

    Clean and correcting alpha and refreshControl statement

--- RuckSack/Controller/WeatherViewController.swift
+++ RuckSack/Controller/WeatherViewController.swift
@@ -16,18 +16,18 @@ class WeatherViewController: UITableViewController, WeatherServiceDelegate {
     
     // MARK: - Outlets
     
-        // MARK: ** TableView __ NavigationBar
+    // MARK: ** TableView __ NavigationBar
     @IBOutlet var tableViewWeather: UITableView!
     @IBOutlet weak var buttonTown: UIBarButtonItem!
-        
-        // MARK: ** TemperatureCell
+    
+    // MARK: ** TemperatureCell
     @IBOutlet weak var imageWeatherState: UIImageView!
     @IBOutlet weak var weatherStateTitle: UILabel!
     @IBOutlet weak var temperatureMax: UILabel!
     @IBOutlet weak var temperatureMin: UILabel!
     @IBOutlet weak var temperatureActual: UILabel!
     
-        // MARK: ** WeekCell
+    // MARK: ** WeekCell
     @IBOutlet weak var actualDayWeather: ActualDayWeather!
     @IBOutlet weak var stackViewState: UIStackView!
     
@@ -56,65 +56,76 @@ class WeatherViewController: UITableViewController, WeatherServiceDelegate {
         //self.tableViewWeather.backgroundView?.alpha = 1
         
     }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return self.view.frame.size.height - (self.navigationController?.navigationBar.frame.size.height)! - (self.tabBarController?.tabBar.frame.size.height)! - 20
+        }
+        return super.tableView(tableView, heightForRowAt: indexPath)
+    }
     
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let indexpath = IndexPath(row: 0, section: 0)
         let rowZeroFrame = tableViewWeather.rectForRow(at: indexpath)
-        print(scrollView.contentOffset.y)
-        let offset = scrollView.contentOffset.y/(self.tableViewWeather.contentSize.height/2 - rowZeroFrame.height/2)
-        self.tableViewWeather.backgroundView?.alpha = 1.5-offset
+        //print("contentOffsety: \(scrollView.contentOffset.y)")
+        let contentZeroPosition = scrollView.contentOffset.y
+//        print("contentSize: \(tableViewWeather.contentSize.height)")
+//        print("height: \(rowZeroFrame.height)")
+        let offset = (scrollView.contentOffset.y + contentZeroPosition)/(self.tableViewWeather.contentSize.height - rowZeroFrame.height)
+        //print("Offset: \(offset)")
+        self.tableViewWeather.backgroundView?.alpha = 1-offset
+        
     }
 }
 
 
 
-    extension WeatherViewController {
-// MARK: - Public Methods
-        // MARK: ** Methods
-   
-        func didUpdateWeatherData(openWeather: OpenWeather) {
-               setStackViewDayState()
-               createTodayState()
-            }
-        func didHappenedError(error: NetworkError) {
-               switch error {
-               case .clientError: self.alert(title: "Internet Connection",
-                                             message: "We cannot etablish an internet connection. Please retry in a moment",
-                                             titleAction: "Ok",
-                                             actionStyle: .default)
-               case .serverError: self.alert(title: "Internet Connection",
-                                             message: "Retry please in a moment",
-                                             titleAction: "Ok",
-                                             actionStyle: .default)
-               case .jsonError: self.alert(title: "Json problem",
-                                           message: "Retry please in a moment",
-                                           titleAction: "Ok",
-                                           actionStyle: .default)
-               }
-               
-           }
-        
-        // MARK: ** Handling Screening Days State
-        
-        func createDayState(number: Int) -> UIView {
-            
-            let dayWeatherState = DayWeatherState()
-            
-            dayWeatherState.dayName.text = weatherService.setDayStateName(indexList: number)
-            dayWeatherState.imageState.image = setStateImage(indexList: number)
-            dayWeatherState.humidityAmount.text = String(weatherService.openWeather!.list[number].main.humidity) + "%"
-            dayWeatherState.temp.text =
-                String(format:"%.f", weatherService.openWeather!.list[number].main.tempMax.celcius) + "°C"
-            return dayWeatherState
+extension WeatherViewController {
+    // MARK: - Public Methods
+    // MARK: ** Methods
+    
+    func didUpdateWeatherData(openWeather: OpenWeather) {
+        setStackViewDayState()
+        createTodayState()
+    }
+    func didHappenedError(error: NetworkError) {
+        switch error {
+        case .clientError: self.alert(title: "Internet Connection",
+                                      message: "We cannot etablish an internet connection. Please retry in a moment",
+                                      titleAction: "Ok",
+                                      actionStyle: .default)
+        case .serverError: self.alert(title: "Internet Connection",
+                                      message: "Retry please in a moment",
+                                      titleAction: "Ok",
+                                      actionStyle: .default)
+        case .jsonError: self.alert(title: "Json problem",
+                                    message: "Retry please in a moment",
+                                    titleAction: "Ok",
+                                    actionStyle: .default)
         }
-        func createTodayState() {
-            
-            actualDayWeather.weatherDescription.text = weatherService.openWeather!.list[0].weather[0].weatherDescription.rawValue.capitalized
-            actualDayWeather.imageActualWeather.image = setStateImage(indexList: 0)
-            actualDayWeather.humidyAmount.text = String(weatherService.openWeather!.list[0].main.humidity) + "%"
-            actualDayWeather.tempActual.text = String(format:"%.f", weatherService.openWeather!.list[0].main.temp.celcius) + "°C"
-        }
-        func setStateImage(indexList: Int) -> UIImage {
+        
+    }
+    
+    // MARK: ** Handling Screening Days State
+    
+    func createDayState(number: Int) -> UIView {
+        
+        let dayWeatherState = DayWeatherState()
+        
+        dayWeatherState.dayName.text = weatherService.setDayStateName(indexList: number)
+        dayWeatherState.imageState.image = setStateImage(indexList: number)
+        dayWeatherState.humidityAmount.text = String(weatherService.openWeather!.list[number].main.humidity) + "%"
+        dayWeatherState.temp.text =
+            String(format:"%.f", weatherService.openWeather!.list[number].main.tempMax.celcius) + "°C"
+        return dayWeatherState
+    }
+    func createTodayState() {
+        
+        actualDayWeather.weatherDescription.text = weatherService.openWeather!.list[0].weather[0].weatherDescription.rawValue.capitalized
+        actualDayWeather.imageActualWeather.image = setStateImage(indexList: 0)
+        actualDayWeather.humidyAmount.text = String(weatherService.openWeather!.list[0].main.humidity) + "%"
+        actualDayWeather.tempActual.text = String(format:"%.f", weatherService.openWeather!.list[0].main.temp.celcius) + "°C"
+    }
+    func setStateImage(indexList: Int) -> UIImage {
         var imageStateDay = UIImage()
         let value = weatherService.openWeather?.list[indexList].weather[0].main
         let description = weatherService.openWeather?.list[indexList].weather[0].weatherDescription
@@ -144,7 +155,7 @@ class WeatherViewController: UITableViewController, WeatherServiceDelegate {
                 }
             }
         case .rain:
-        
+            
             if description!.rawValue == "freezing rain" {
                 imageStateDay = UIImage(named: "13d")!
             }
@@ -178,110 +189,119 @@ class WeatherViewController: UITableViewController, WeatherServiceDelegate {
         }
         return imageStateDay
     }
-        
-        // MARK: ** Handling Town Change
-        
-        func switchTown() {
-            let townActual = backgroundDefault.string(forKey: "town")
-            switch townActual {
-            case "Berlin":
-                backgroundDefault.set("NewYork", forKey: "town")
-            case "NewYork":
-                backgroundDefault.set("Berlin", forKey: "town")
-            default:
-                print("Something wrong happen")
-            }
-        }
-        func setTown(town: UserDefaults) {
-            let townName = backgroundDefault.string(forKey: "town")
-            switch townName {
-            case "Berlin":
-                buttonTown.image = UIImage(named: "tv_tower")
-                tableViewWeather.backgroundView = UIImageView(image: UIImage(named: "Background_Weather_Berlin"))
-                navigationItem.title = "Berlin"
-                print("Berlin is refresh")
-                self.weatherService.askWeatherState(town: self.weatherService.berlin)
-               
-            case "NewYork":
-                self.buttonTown.image = UIImage(named: "liberty")
-                tableViewWeather.backgroundView = UIImageView(image: UIImage(named: "Background_Weather_NewYork"))
-                navigationItem.title = "New-York"
-                print("New-York is refresh")
-                self.weatherService.askWeatherState(town: self.weatherService.newYork)
-            default:
-                print("erreur")
-           }
-            tableViewWeather.backgroundView?.contentMode = .scaleAspectFit
-            //repositionCell()
-            self.tableViewWeather.backgroundView?.alpha = 1
-        }
-
-        // MARK: ** Action Methods
-        
-        @IBAction func changeTown(_ sender: Any) {
-               
-               let changeTownAlert = UIAlertController(title: "Change Town?", message: "Do You want to change the town?", preferredStyle: .alert)
-               changeTownAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
-                   self.switchTown()
-                   self.setTown(town: self.backgroundDefault)
-                   self.repositionCell()
-                    self.tableViewWeather.backgroundView?.alpha = 1
-                   
-               }))
-               changeTownAlert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
-               
-               self.present(changeTownAlert, animated: true, completion: nil)
-           }
-        @objc func handleSwipes(_ refreshControl: UIRefreshControl) {
-////            self.setTown(town: self.backgroundDefault)
-////            //let deadline = DispatchTime.now() + .milliseconds(700)
-////            DispatchQueue.main.async {
-////                self.refreshControl?.endRefreshing()
-////
-////                self.tableViewWeather.reloadData()
-////
-////                }
-//            self.setTown(town: backgroundDefault)
-//
-//            self.repositionCell()
-            if !tableViewWeather.isDragging {
-                refreshWeatherViewController()
-            }
-            repositionCell()
-         }
-        func refreshWeatherViewController() {
-            self.refreshControl?.endRefreshing()
-            
-            setTown(town: backgroundDefault)
-            let deadline = DispatchTime.now() + .seconds(1)
-            DispatchQueue.main.asyncAfter(deadline: deadline) {
-                self.tableViewWeather.reloadData()
-                //repositionCell()
-            }
-            
-        }
-        override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-            if refreshControl?.isRefreshing == true {
-                refreshWeatherViewController()
-            }
+    
+    // MARK: ** Handling Town Change
+    
+    func switchTown() {
+        let townActual = backgroundDefault.string(forKey: "town")
+        switch townActual {
+        case "Berlin":
+            backgroundDefault.set("NewYork", forKey: "town")
+        case "NewYork":
+            backgroundDefault.set("Berlin", forKey: "town")
+        default:
+            print("Something wrong happen")
         }
     }
+    func setTown(town: UserDefaults) {
+        let townName = backgroundDefault.string(forKey: "town")
+        switch townName {
+        case "Berlin":
+            buttonTown.image = UIImage(named: "tv_tower")
+            tableViewWeather.backgroundView = UIImageView(image: UIImage(named: "Background_Weather_Berlin"))
+            navigationItem.title = "Berlin"
+            print("Berlin is refresh")
+            self.weatherService.askWeatherState(town: self.weatherService.berlin)
+            
+        case "NewYork":
+            self.buttonTown.image = UIImage(named: "liberty")
+            tableViewWeather.backgroundView = UIImageView(image: UIImage(named: "Background_Weather_NewYork"))
+            navigationItem.title = "New-York"
+            print("New-York is refresh")
+            self.weatherService.askWeatherState(town: self.weatherService.newYork)
+        default:
+            print("erreur")
+        }
+        tableViewWeather.backgroundView?.contentMode = .scaleAspectFit
+        //repositionCell()
+        self.tableViewWeather.backgroundView?.alpha = 1
+    }
+    
+    // MARK: ** Action Methods
+    
+    @IBAction func changeTown(_ sender: Any) {
+        
+        let changeTownAlert = UIAlertController(title: "Change Town?", message: "Do You want to change the town?", preferredStyle: .alert)
+        changeTownAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+            self.switchTown()
+            self.setTown(town: self.backgroundDefault)
+            self.repositionCell()
+            self.tableViewWeather.backgroundView?.alpha = 1
+            
+        }))
+        changeTownAlert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+        
+        self.present(changeTownAlert, animated: true, completion: nil)
+    }
+//    @objc func handleSwipes(_ refreshControl: UIRefreshControl) {
+//        ////            self.setTown(town: self.backgroundDefault)
+//        ////            //let deadline = DispatchTime.now() + .milliseconds(700)
+//        ////            DispatchQueue.main.async {
+//        ////                self.refreshControl?.endRefreshing()
+//        ////
+//        ////                self.tableViewWeather.reloadData()
+//        ////
+//        ////                }
+//        //            self.setTown(town: backgroundDefault)
+//        //
+//        //            self.repositionCell()
+//        if !tableViewWeather.isDragging {
+//            refreshWeatherViewController()
+//        }
+//        repositionCell()
+//    }
+    @objc func handleSwipes(_ sender: Any) {
+        self.setTown(town: backgroundDefault)
+        self.refreshControl?.endRefreshing()
+    DispatchQueue.main.async {
+    self.tableViewWeather.reloadData()
+    //self.refreshControl?.endRefreshing()
+
+    }
+}
+//    func refreshWeatherViewController() {
+//        self.refreshControl?.endRefreshing()
+//
+//        setTown(town: backgroundDefault)
+//        let deadline = DispatchTime.now() + .seconds(1)
+//        DispatchQueue.main.asyncAfter(deadline: deadline) {
+//            self.tableViewWeather.reloadData()
+//            //repositionCell()
+//        }
+//
+//    }
+//    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+//        if refreshControl?.isRefreshing == true {
+//            refreshWeatherViewController()
+//        }
+//    }
+}
 // MARK: - Private Methods
 
-    // MARK: ** View Manipulation
+// MARK: ** View Manipulation
 
 extension WeatherViewController {
     
     fileprivate func setStackViewDayState() {
-    if stackViewState.arrangedSubviews.count != 0 {
-        resetStackViewDayState()
+        if stackViewState.arrangedSubviews.count != 0 {
+            resetStackViewDayState()
+        }
+        stackViewState.insertArrangedSubview(createDayState(number: 8), at: 0)
+        stackViewState.insertArrangedSubview(createDayState(number: 16), at: 1)
+        stackViewState.insertArrangedSubview(createDayState(number: 24), at: 2)
+        stackViewState.insertArrangedSubview(createDayState(number: 32), at: 3)
+        stackViewState.insertArrangedSubview(createDayState(number: 39), at: 4)
     }
-    stackViewState.insertArrangedSubview(createDayState(number: 8), at: 0)
-    stackViewState.insertArrangedSubview(createDayState(number: 16), at: 1)
-    stackViewState.insertArrangedSubview(createDayState(number: 24), at: 2)
-    stackViewState.insertArrangedSubview(createDayState(number: 32), at: 3)
-    stackViewState.insertArrangedSubview(createDayState(number: 39), at: 4)
-}
     
     fileprivate func resetStackViewDayState() {
         stackViewState.subviews.forEach { (view) in
@@ -290,13 +310,13 @@ extension WeatherViewController {
     }
     
     fileprivate func repositionCell() {
-        let indexPath: IndexPath = IndexPath(row: 1, section: 0)
+        let indexPath: IndexPath = IndexPath(row: 0, section: 0)
         self.tableViewWeather.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
     
     fileprivate func setRefreshControl() {
-           let titleAttribute = [NSAttributedString.Key.foregroundColor: UIColor.blue,
-                                 NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16)]
+        let titleAttribute = [NSAttributedString.Key.foregroundColor: UIColor.blue,
+                              NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 16)]
         self.refreshControl!.tintColor = UIColor.white
         self.refreshControl!.backgroundColor =  UIColor(displayP3Red: 0.612, green: 0.804, blue: 0.91, alpha: 1)
         self.refreshControl!.attributedTitle = NSAttributedString(string: "Pull down for reloading", attributes: titleAttribute )
